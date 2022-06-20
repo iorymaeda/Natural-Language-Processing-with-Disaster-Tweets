b427f17b81e1e7af32f0b4c7964dff75be042dae
iorymaeda <86793373+iorymaeda@users.noreply.github.com>
2022-06-20 04:48:38 +0300

    add new dataset class

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,7 @@
+import random
+
+import numpy as np
+import torch
 from torch.utils.data import Dataset
 
 
@@ -16,3 +20,32 @@ class EmbeddingDataset(Dataset):
         x = self.x[idx]
         y = self.y[idx]
         return x, y
+
+
+
+class NLPDataset(Dataset):
+    def __init__(self, x, y, rand=False, **kwargs):
+        """`rand` will balance classes"""
+        self.x = x
+        self.y = torch.LongTensor(y)      
+        
+        self.rand = rand
+        if rand:
+            self.classes = list(set(y))
+            self.indeces = np.arange(0, len(y))
+    
+    def __len__(self):
+        return len(self.y)
+    
+    
+    def __getitem__(self, idx):
+        if self.rand:
+            target_class = random.choice(self.classes)
+            idx = self.indeces[self.y == target_class]
+            idx = random.choice(idx)
+            
+        x = {k: v[idx] for k, v in self.x.items()}
+        y = self.y[idx]
+        return y, x['input_ids'], x['attention_mask'], x['token_type_ids']
+
+
